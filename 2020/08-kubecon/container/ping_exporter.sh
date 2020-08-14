5b7510e18e54ce3885e0ff83c6ab8bcc2b381668
Andrey Klimentyev <andrey.klimentyev@flant.com>
2020-08-14 13:44:13 +0300

    Kubecon 2020 (#10)

---
+++ 2020/08-kubecon/container/ping_exporter.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+source /shell_lib.sh
+
+function __config__() {
+  cat << EOF
+    configVersion: v1
+    kubernetes:
+    - name: nodes
+      apiVersion: v1
+      kind: Node
+      jqFilter: |
+        {
+          name: .metadata.name,
+          ip: (
+          .status.addresses[] |
+            select(.type == "InternalIP") |
+            .address
+          )
+        }
+      group: main
+      keepFullObjectsInMemory: false
+      executeHookOnEvent: []
+    schedule:
+    - name: every_minute
+      group: main
+      crontab: "* * * * *"
+EOF
+}
+
+function __main__() {
+  for i in $(seq 0 "$(context::jq -r '(.snapshots.nodes | length) - 1')"); do
+    node_name="$(context::jq -r '.snapshots.nodes['"$i"'].filterResult.name')"
+    node_ip="$(context::jq -r '.snapshots.nodes['"$i"'].filterResult.ip')"
+
+    packets_lost=0
+    if ! ping -c 1 "$node_ip" -t 1 ; then
+      packets_lost=1
+    fi
+
+    cat >> "$METRICS_PATH" <<END
+      {
+        "name": "node_packets_lost",
+        "add": $packets_lost,
+        "labels": {
+          "node": "$node_name"
+        }
+      }
+END
+
+  done
+}
+
+hook::run "$@"
---
+++ 2020/08-kubecon/container/secret_copier.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+source /shell_lib.sh
+
+function __config__() {
+  cat << EOF
+    configVersion: v1
+    kubernetes:
+    - name: src_secret
+      apiVersion: v1
+      kind: Secret
+      nameSelector:
+        matchNames:
+        - mysecret
+      namespace:
+        nameSelector:
+          matchNames: ["default"]
+      group: main
+    - name: dst_secrets
+      apiVersion: v1
+      kind: Secret
+      labelSelector:
+        matchLabels:
+          managed-secret: "yes"
+      jqFilter: |
+        {
+          "namespace": .metadata.namespace,
+          "resourceVersion": .annotations."resourceVersion"
+        }
+      group: main
+      keepFullObjectsInMemory: false
+    - name: namespaces
+      group: main
+      apiVersion: v1
+      kind: Namespace
+      jqFilter: |
+        {
+          name: .metadata.name,
+          hasLabel: (.metadata.labels // {} | contains({"secret": "yes"}))
+        }
+      group: main
+      keepFullObjectsInMemory: false
+EOF
+}
+
+function sync_secret() {
+  src_resource_version="$(context::jq -r '.snapshots.src_secret[0].object.metadata.resourceVersion')"
+  dst_resource_version="$(context::jq -r '.snapshots.dst_secrets[].filterResult | select(.namespace == "'"$1"'") | .resourceVersion')"
+
+  if [ "$src_resource_version" != "$dst_resource_version" ] ; then
+    new_secret="$(context::jq -r '.snapshots.src_secret[0].object | . |= (.metadata =
+                                                                            {"name": .metadata.name, "namespace": "'"$1"'",
+                                                                            "labels": {"managed-secret": "yes"},
+                                                                            "annotations": {"resourceVersion": .metadata.resourceVersion}})')"
+
+    kubectl -n "$1" replace -f <(echo "$new_secret") || kubectl -n "$1" create -f  <(echo "$new_secret")
+  fi
+}
+
+function delete_secret() {
+  if context::jq -e --arg ns "$1" '.snapshots.dst_secrets[].filterResult | select(.namespace == $ns)' ; then
+    if [[ "$1" == "default" ]]; then
+      return 0
+    fi
+
+    kubectl -n "$1" delete secret "mysecret"
+  fi
+}
+
+function __main__() {
+  for i in $(seq 0 "$(context::jq -r '(.snapshots.namespaces | length) - 1')"); do
+    ns_name="$(context::jq -r '.snapshots.namespaces['"$i"'].filterResult.name')"
+    if context::jq -e '.snapshots.namespaces['"$i"'].filterResult.hasLabel' ; then
+      sync_secret "$ns_name"
+    else
+      delete_secret "$ns_name"
+    fi
+  done
+}
+
+hook::run "$@"
